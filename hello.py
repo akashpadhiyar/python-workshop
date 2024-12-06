c681e351b395eb1b81af8f29ed888bc75c473838
Akash Padhiyar <akashpadhiyar@users.noreply.github.com>
2024-12-06 13:29:37 +0530

    V1
    First Commit from Indus University

---
+++ hello.py
@@ -0,0 +1,12 @@
+sum = 0
+for i  in range(0,3):
+    a = int(input("Enter Marks:"))
+    sum += a 
+avg = sum / 3
+print("Sum is " , sum)   
+print("Avg is " , avg)   
+
+if avg > 80:
+    print("Pass")
+else:
+    print("Fail")
---
+++ pro2.py
@@ -0,0 +1,6 @@
+def myfunction(name):
+    print("Hi",name)
+    
+myfunction("Daya")
+myfunction("Jetha")
+myfunction("PopatLal")
